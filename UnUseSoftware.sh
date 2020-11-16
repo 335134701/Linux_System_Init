915b62bdf0073121faa24b1f8be1c4ff5fbd4889
zc <335134701@qq.com>
2020-11-16 21:12:59 +0800

    2020-11-16 21:12:59 : 优化UnUseSoftware.sh脚本函数结构

--- UnUseSoftware.sh
+++ UnUseSoftware.sh
@@ -13,6 +13,21 @@ function Check_Library()
 	fi
 	echo
 }
+#处理函数
+function Remove(){
+    if [ ${#} -eq 1 ]; then
+        if [ ${systemName} = "Raspbian" -o  ${systemName} = "Ubuntu" ]; then
+            sudo apt-get autoremove ${1} -y
+            Judge_Order "sudo apt-get autoremove ${1} -y" 1
+        elif [ ${systemName} = "CentOS" ]; then
+            sudo yum autoremove ${1} -y
+            Judge_Order "sudo yum autoremove ${1} -y" 1
+        fi
+    else
+        Log -E "函数参数输入有误!"
+    fi
+    
+}
 #以下函数还可以进行优化处理，优化方式是形成函数
 #卸载Ubuntu默认，无用软件
 function Remove_Unusing_Software(){
@@ -20,52 +35,32 @@ function Remove_Unusing_Software(){
         Log -I "暂未找到无用软件!"
     elif [ ${systemName} = "Ubuntu" ];then
         #雷鸟邮件客户端
-        sudo apt-get remove thunderbird -y
-        Judge_Order "sudo apt-get remove thunderbird -y" 1
+        Remove "thunderbird"
         #自带播放器
-        sudo apt-get remove totem -y
-        Judge_Order "sudo apt-get remove totem -y" 1
+        Remove "totem"
         #扫描仪
-        sudo apt-get remove simple-scan -y
-        Judge_Order "sudo apt-get remove simple-scan -y" 1
-        #纸牌游戏
-        sudo apt-get remove aisleriot -y
-        Judge_Order "sudo apt-get remove aisleriot -y" 1
+        Remove "simple-scan"
         #数独游戏
-        sudo apt-get remove gnome-sudoku -y
-        Judge_Order "sudo apt-get remove gnome-sudoku -y" 1
+        Remove "gnome-sudoku"
         #对对碰游戏
-        sudo apt-get remove gnome-mahjongg -y
-        Judge_Order "sudo apt-get remove gnome-mahjongg -y" 1
+        Remove "gnome-mahjongg"
         #扫雷游戏
-        sudo apt-get remove gnome-mines -y
-        Judge_Order "sudo apt-get remove gnome-mines  -y" 1
+        Remove "gnome-mines"
         #备份
-        sudo apt-get remove deja-dup -y
-        Judge_Order "sudo apt-get remove deja-dup -y" 1
+        Remove "deja-dup"
         #Amazon商店
-        sudo apt-get remove unity-webapps-common -y
-        Judge_Order "sudo apt-get remove unity-webapps-common -y" 1
+        Remove "unity-webapps-common"
         #自带的音乐播放器
-        sudo apt-get remove rhythmbox -y
-        Judge_Order "sudo apt-get remove rhythmbox -y" 1
+        Remove "rhythmbox"
         #自带的即时聊天应用
-        sudo apt-get remove empathy -y
-        Judge_Order "sudo apt-get remove empathy -y" 1
+        Remove "empathy"
         if [ ${systemVersion} = "14" ];then
-            sudo apt-get remove brasero -y
-            Judge_Order "sudo apt-get remove brasero -y" 1
+            Remove "brasero"
             #软件中心
-            sudo apt-get remove software-center -y
-            Judge_Order "sudo apt-get remove software-center -y" 1
-        elif [ ${systemVersion} = "16" ];then
+            Remove "software-center"
+        elif [ ${systemVersion} = "16" -o ${systemVersion} = "18" ];then
             #软件中心
-            sudo apt-get remove gnome-software -y
-            Judge_Order "sudo apt-get remove gnome-software -y" 1
-        elif [ ${systemVersion} = "18" ];then
-             #软件中心
-            sudo apt-get remove gnome-software -y
-            Judge_Order "sudo apt-get remove gnome-software -y" 1
+            Remove "gnome-software"
         elif [ ${systemVersion} = "20" ];then
             Log -D "还未找到卸载软件中心的办法!"
         fi
