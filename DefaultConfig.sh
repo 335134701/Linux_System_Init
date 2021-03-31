234a086ae172336142ffb38cb8e94ff6133da987
zc <335134701@qq.com>
2021-03-31 21:05:48 +0800

    2021-03-31 21:05:48 : 添加防火墙

--- DefaultConfig.sh
+++ DefaultConfig.sh
@@ -150,6 +150,53 @@ function Config_Vsftpd(){
 	Log -I "Config_Vsftpd() 函数执行完成!"
 	echo	
 }
+#配置防火墙
+function Config_Iptables(){
+	case "${systemName}" in
+		Raspbian)
+			#启用防火墙
+			sudo ufw enable
+			#关闭外部对本机访问   
+			sudo ufw default deny
+			Judge_Order "sudo ufw default deny" 0
+			sudo ufw allow ${ConfigArray[sshport]}
+			Judge_Order "sudo ufw allow ${ConfigArray[sshport]}" 0
+			sudo ufw allow ${ConfigArray[vsftpport]}
+			Judge_Order "sudo ufw allow ${ConfigArray[vsftpport]}" 0
+			sudo ufw allow ${ConfigArray[vsftpdataport]}
+			Judge_Order "sudo ufw allow ${ConfigArray[vsftpdataport]}" 0
+			sudo ufw allow ${ConfigArray[mysqlport]}
+			Judge_Order "sudo ufw allow ${ConfigArray[mysqlport]}" 0
+			sudo ufw allow ${ConfigArray[vncserverport]}
+			Judge_Order "sudo ufw allow ${ConfigArray[vncserverport]}" 0
+			#以下是防火墙相关操作说明:
+				#sudo ufw allow 80 允许外部访问80端口
+				#sudo ufw delete allow 80 禁止外部访问80 端口
+				#sudo ufw allow from 192.168.1.1 允许此IP访问所有的本机端口
+				#sudo ufw deny smtp 禁止外部访问smtp服务
+				#sudo ufw delete allow smtp 删除上面建立的某条规则
+				#ufw deny proto tcp from 10.0.0.0/8 to 192.168.0.1 port要拒绝所有的流量从TCP的10.0.0.0/8 到端口22的地址192.168.0.1
+				#可以允许所有RFC1918网络（局域网/无线局域网的）访问这个主机（/8,/16,/12是一种网络分级）：
+				#sudo ufw allow from 10.0.0.0/8
+				#sudo ufw allow from 172.16.0.0/12
+				#sudo ufw allow from 192.168.0.0/16
+				#配置允许的端口范围 
+				#sudo ufw allow 6000:6007/tcp 
+				#sudo ufw allow 6000:6007/udp
+			;;
+		Ubuntu)
+			Log -D "调试中。。。。。。"
+		;;
+		CentOS)
+			Log -D "调试中。。。。。。"
+		;;
+		*)
+			Log -E "未知系统!"
+		;;
+	esac
+	Log -I "Config_Iptables() 函数执行完成!"
+	echo
+}
 function Raspbian_Config(){
 	#第1步:开启vncserver,需要注意如下几点:
 	#需要注意的是:此服务在raspi-config 中设置好后默认是启动(若服务未启动，可以重启系统)，无需再手动启动，若默认启动失败，则可以手动启动
@@ -186,6 +233,9 @@ function Raspbian_Config(){
 		Default_Install  "vsftpd" && Config_Vsftpd && \
 		sudo service vsftpd restart && \
 		Judge_Order "sudo service vsftpd restart" 0
+	#第八步:配置防火墙
+	test ! -f '/usr/sbin/ufw' && Default_Install  "ufw"
+	Config_Iptables
 }
 function Ubuntu_Config(){
 	#设置为no，更改默认dash为bash
